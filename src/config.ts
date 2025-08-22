978937a2b9ae3409e895f74f242793c9f3f13ddf
Marten <gierth.marten@gmail.com>
2025-08-22 20:15:05 +0200

    Refactor: Centralize URL paths and update moxr-glb URL

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+export const urls = {
+  moxr: 'https://j0nesyes.github.io/moxr',
+  moxrExplorer: 'https://j0nesyes.github.io/moxr/#explorer',
+  moxrGlbViewer: 'https://j0nesyes.github.io/moxr-glb-viewer/?file=',
+  moxrGlb: 'https://j0nesyes.github.io/moxr/',
+  mindObjectsImprint: 'https://mind-objects.com/imprint/',
+  icuMedia: 'https://icu-media.xyz/',
+};
