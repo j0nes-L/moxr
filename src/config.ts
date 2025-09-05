20a5f7b29994861588f11984f051eeb133ddb45b
Marten <gierth.marten@gmail.com>
2025-09-05 15:39:05 +0200

    Add YouTube trailer section to IndexPage
    Introduces a new trailer section on the IndexPage featuring an embedded YouTube video. Updates config to include the YouTube trailer ID and adds corresponding CSS styles for the trailer iframe.

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,7 @@
 export const urls = {
   moxr: 'https://j0nes-l.github.io/moxr',
   metaBeta: 'https://www.meta.com/s/4JKJaWuBi',
+  moxrYtTrailerID:"qE8rGF05sg0",
   mindObjectsImprint: 'https://mind-objects.com/imprint/',
   icuMedia: 'https://icu-media.xyz/',
 };
